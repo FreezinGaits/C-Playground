ef4094560e0684a1a312b46cdf395fb29decac26
FreezinGaits <freezinggaits917@gmail.com>
2024-11-22 00:24:46 +0530

    Merge branch 'main' of https://github.com/FreezinGaits/Hashing

--- hashing/+ve_-ve_pair.cpp
+++ hashing/+ve_-ve_pair.cpp
@@ -4,17 +4,23 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
-public: 
-    vector<int> PosNegPair(int a[], int n) {
+class Solution
+{
+public:
+    vector<int> PosNegPair(int a[], int n)
+    {
         vector<int> negatives, result;
         map<int, int> positiveCount;
 
         // Separate negative numbers and count positive numbers
-        for (int i = 0; i < n; i++) {
-            if (a[i] < 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] < 0)
+            {
                 negatives.push_back(a[i]);
-            } else {
+            }
+            else
+            {
                 positiveCount[a[i]]++;
             }
         }
@@ -23,9 +29,11 @@ public:
         sort(negatives.begin(), negatives.end());
 
         // Find pairs of negative and positive numbers
-        for (int i = (int)negatives.size() - 1; i >= 0; i--) {
+        for (int i = (int)negatives.size() - 1; i >= 0; i--)
+        {
             int data = abs(negatives[i]);
-            if (positiveCount[data] > 0) {
+            if (positiveCount[data] > 0)
+            {
                 result.push_back(negatives[i]);
                 result.push_back(data);
                 positiveCount[data]--;
@@ -36,13 +44,15 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     int arr[] = {3, -3, -2, 2, 1, -1, -4, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     vector<int> result = sol.PosNegPair(arr, n);
-    for (int i : result) {
+    for (int i : result)
+    {
         cout << i << " ";
     }
     cout << endl;
